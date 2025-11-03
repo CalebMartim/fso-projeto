0e70911f3e85f16108e6da27f1babfa2011d08d1
Guilherme <guilherme.r.cunha.1@gmail.com>
2025-11-03 17:34:56 -0300

    implementação básica da leitura dos comandos

--- source/main.c
+++ source/main.c
@@ -1,34 +1,73 @@
+/*
+Nomes:
+  Caleb Martim -- 221017060
+  Gabriel Henrique do Nascimento -- 221029140
+  Guilherme da Rocha -- 221030007
+  Hiago Sousa -- 221002049
+
+Compilador: gcc 13.3.0
+
+Sistema Operacional: LINUX 6.14.0-33-generic
+*/
+
+
 #include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
 
 #define true 1
 #define false 0
 
-char *quit = "quit";
-
-int len(char *s) {
-  int n = 0;
-  while (s[n] != '\0')
-    ++n;
-  return n;
-}
-
-int verify(char *aux) {
-  int i = 0;
-  while (aux[i] != '\0' && aux[i] == quit[i]) 
-    ++i;
-  if (i == len(quit) && aux[i] == '\0') 
-    return 0;
-
-  return 1;
-}
+const char *exit_s = "exit_scheduler";
+const char *list_s = "list_scheduler";
+const char *exec_s = "execute_process";
+const char *user_s = "user_scheduler";
 
 int main(){
-  char aux[100];
+  char command[100];
+
   while (true) {
     printf(">shell_sched: ");
-    scanf("%s", aux);
-    printf("%d\n", verify(aux));
-    if (verify(aux) == 0) 
-      return 0;
+    
+    if(fgets(command, sizeof(command), stdin) == NULL) { // fgets le toda a linha
+      fprintf(stderr, "Erro: falha ao ler o comando.\n");
+      exit(EXIT_FAILURE);
+    }
+    command[strcspn(command,"\n")] = '\0'; // remove o '\n'
+
+    if(strcmp(command, exit_s) == 0) break;
+    
+    if(strcmp(command, list_s) == 0) {
+      printf("Lista de processos: [...]\n");
+
+      printf("Processo em execução: [...]\n");
+
+      continue;
+    }
+
+    char *token = strtok(command, " ");
+    if(token != NULL) {
+      if(strcmp(token, user_s) == 0) {
+        token = strtok(NULL, " "); // numero de listas
+
+        printf("Criando %s filas round-robin.\n",token);
+
+        continue;
+      }
+
+      if(strcmp(token, exec_s) == 0) {
+        token = strtok(NULL, " "); // prioridade
+
+        printf("Executando processos de prioridade %s.\n",token);
+
+        continue;
+      }
+
+    }
+
+    fprintf(stderr,"Erro: Comando não definido.\n");
+    exit(EXIT_FAILURE); // exit(1) | return 1
   }
+
+  return EXIT_SUCCESS; // return 0
 }
